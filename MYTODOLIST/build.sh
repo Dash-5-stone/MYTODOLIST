e657f61b2572295faf3a207bc0497d649fb51cea
Dash-5-stone <151950672+Dash-5-stone@users.noreply.github.com>
2025-04-25 14:33:07 +0000

    Update build.sh

--- MYTODOLIST/build.sh
+++ MYTODOLIST/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+set -o errexit  # exit on error
+
 python -m pip install --upgrade pip
+pip install poetry==1.7.1
+poetry export --without-hashes -f requirements.txt -o requirements.txt
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
